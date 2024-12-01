343f7146cb186bb13138e2bf38886e0c5abcf286
Ben Arnold <Ben@Bens-MacBook-Air.local>
2024-12-01 13:07:31 -0500

    2023 day 9 pt2

--- 2023/09/09.py
+++ 2023/09/09.py
@@ -2,13 +2,17 @@ import numpy as np
 
 data = np.loadtxt("2023/09/input.txt", dtype=int)
 
-def recursive(d, debug=False):
+def recursive(d, part2=False, debug=False):
     if debug: print(d)
     if set(d) == {0}: return 0
-    return recursive(np.diff(d), debug) + d[-1]
+    if part2: 
+        return d[0] - recursive(np.diff(d), part2, debug) # doesn't work
+    else:
+        return recursive(np.diff(d), part2, debug) + d[-1]
 
+#print(sum([recursive(d) for d in data]))
 
 print(sum([recursive(d) for d in data]))
-#test = recursive(data[1], True)
-#print(test)
-#print(len(data[1]), len(np.diff(data[1])))
+print(sum([recursive(d[::-1]) for d in data]))
+
+
