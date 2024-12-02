484e9f937ee328be1df71120b534c8ee170a3f28
Ben Arnold <Ben@Bens-MacBook-Air.local>
2024-12-01 22:20:38 -0500

    2023 day 11 pt2

--- 2023/11/11.py
+++ 2023/11/11.py
@@ -1,6 +1,6 @@
 import itertools
 
-univ = open("2023/11/test.txt").read().strip()
+univ = open("2023/11/input.txt").read().strip()
 univ = univ.replace('.', '0').replace('#', '1')
 univ = [list(map(int, list(row))) for row in univ.split('\n')]
 
@@ -8,20 +8,17 @@ def transpose(matrix):
     return [list(row) for row in zip(*matrix)]
 
 def expand_universe(univ, factor=1):
-    expanded_rows = []
-    expanded_universe = []
-    for row in univ:
+    new_rows = list(range(len(univ)))
+    new_cols = list(range(len(univ[0])))
+    for i, row in enumerate(univ):
         if sum(row) == 0:
-            expanded_rows.extend([row] * factor)
-        expanded_rows.append(row)
-    for col in transpose(expanded_rows):
+            for j in range(i, len(new_rows)):
+                new_rows[j] += factor
+    for i, col in enumerate(transpose(univ)):
         if sum(col) == 0:
-            expanded_universe.extend([col] * factor)
-        expanded_universe.append(col)
-    return transpose(expanded_universe)
-
-def univ_to_str(univ):
-    return '\n'.join([''.join(map(str, row)) for row in univ])
+            for j in range(i, len(new_cols)):
+                new_cols[j] += factor
+    return new_rows, new_cols
 
 def find_galaxies(univ):
     galaxies = []
@@ -31,20 +28,13 @@ def find_galaxies(univ):
                 galaxies.append((r, c))
     return [(i+1, galaxy) for i, galaxy in enumerate(galaxies)]
 
-def shortest_path(x1, y1, x2, y2):
-    return abs(x1 - x2) + abs(y1 - y2)
+def shortest_path(g1, g2, row_map, col_map):
+    return abs(row_map[g1[0]] - row_map[g2[0]]) + abs(col_map[g1[1]] - col_map[g2[1]])
 
-#print(univ)
-#print("\n")
-expanded_univ = expand_universe(univ)
-#print(univ_to_str(expanded_univ))
-#print("\n")
+galaxies = find_galaxies(univ)
 
-galaxies = find_galaxies(expanded_univ)
-#print("Galaxies:", galaxies)
+rows_pt1, cols_pt1 = expand_universe(univ, 1)
+rows_pt2, cols_pt2 = expand_universe(univ, 1000000-1)
 
-# Iterate through all pairs of galaxies
-#for (i1, g1), (i2, g2) in itertools.combinations(galaxies, 2):
-#    print(f"Pair: Galaxy {i1} at {g1} and Galaxy {i2} at {g2}, Shortest Path: {shortest_path(g1[0], g1[1], g2[0], g2[1])}")
-print(sum([shortest_path(g1[0], g1[1], g2[0], g2[1]) for (i1, g1), (i2, g2) in itertools.combinations(galaxies, 2)]))
-print(sum([shortest_path(g1[0], g1[1], g2[0], g2[1]) for (i1, g1), (i2, g2) in itertools.combinations(find_galaxies(expand_universe(univ,100000-1)), 2)]))
+print(sum(shortest_path(g1, g2, rows_pt1, cols_pt1) for (i1, g1), (i2, g2) in itertools.combinations(galaxies, 2)))
+print(sum(shortest_path(g1, g2, rows_pt2, cols_pt2) for (i1, g1), (i2, g2) in itertools.combinations(galaxies, 2)))
