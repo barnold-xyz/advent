76735022614e1c645bcdff55010b4b3a168d6229
Ben Arnold <Ben@Bens-MacBook-Air.local>
2024-12-02 21:58:38 -0500

    2023 day 13 giveup

--- 2023/13/13.py
+++ 2023/13/13.py
@@ -9,57 +9,49 @@ def compare_lists(l1, l2):
 
 def find_reflection(grid):
     for i, row in enumerate(grid[1:], start=1):
-        first_half = grid[:i]
-        first_half = first_half[::-1]
+        first_half = grid[:i][::-1]
         second_half = grid[i:]
-        if compare_lists(first_half, second_half): return i
+        if compare_lists(first_half, second_half):
+            return i
     return 0
 
 def summarize_reflections(grid):
-    #transposed_grid = list(map(list, zip(*grid)))
     transposed_grid = [''.join(row) for row in zip(*grid)]
     horiz_mirror = find_reflection(grid)
     vert_mirror = find_reflection(transposed_grid)
-    print(horiz_mirror, vert_mirror)
-    return 100*horiz_mirror + vert_mirror
+    return 100 * horiz_mirror + vert_mirror
+
+def part2(grid):
+    def check_reflection(grid, find_reflection_func, any_ok=False):
+        original_mirror = find_reflection_func(grid)
+        for x, row in enumerate(grid):
+            #if x == 0: continue
+            for y, char in enumerate(row):
+                #if y == 0: continue
+                new_grid = grid.copy()
+                new_row = list(new_grid[x])
+                new_row[y] = '.' if new_row[y] == '#' else '#'
+                new_grid[x] = ''.join(new_row)
+                new_mirror = find_reflection_func(new_grid)
+                if any_ok and new_mirror:
+                    return new_mirror
+                if (new_mirror != original_mirror) and (x+y) and new_mirror:
+                    #print(f'FOUND smudge at {x}, {y}, new mirror: {new_mirror}')
+                    #print('\n'.join(['  '.join(pair) for pair in zip(grid, new_grid)]))
+                    return new_mirror
+        return 0
+
+    horiz_mirror = check_reflection(grid, find_reflection)
+    transposed_grid = [''.join(row) for row in zip(*grid)]
+    vert_mirror = check_reflection(transposed_grid, find_reflection, not horiz_mirror)
+
+    #print(horiz_mirror, vert_mirror)
+    return 100 * horiz_mirror + vert_mirror
 
 print(sum(summarize_reflections(grid) for grid in data))
-#[summarize_reflections(grid) for grid in data]
-#summarize_reflections(data[5])
-
-def t(grid):
-    return [''.join(row) for row in zip(*grid)]
-
-#print('\n'.join(data[5]))
-#print('\n'.join(t(data[5])))
-
-def find_reflection2(grid):
-    debug = False
-    print(grid)
-    for i, row in enumerate(grid):
-        print(i, row)
-        debug = i == 9
-        for j, next_row in enumerate(grid[i+1:]):
-            if debug: print(i, j, 'comparing', grid[i], grid[i+j+1])
-            if grid[i] != grid[i+j+1]: break
-            if j == len(grid) - i - 2: return i + 1
-    return 0
-
-
-def find_reflection3(grid):
-    for i, row in enumerate(grid[1:], start=1):
-        first_half = grid[:i]
-        first_half = first_half[::-1]
-        second_half = grid[i:]
-        if compare_lists(first_half, second_half): return i
-    return 0
-
-#print(find_reflection3(data[0]))
-#print([find_reflection3(grid) for grid in data])
-
-            
-
-#print(find_reflection(data[5])) 
-#print(find_reflection2(t(data[5])))
-#print('====')
-#print('\n'.join(t(data[5])))
+print(sum(part2(grid) for grid in data))
+#print(part2(data[3]))
+#test = data[3]
+#print(part2(test))
+#test = [''.join(row) for row in zip(*test)]
+#print(part2(test))
