9920db7e5646eabe723c27c7c120646c92f08add
Ben Arnold <Ben@Bens-MacBook-Air.local>
2024-12-03 17:41:32 -0500

    2023 day 16 pt2

--- 2023/16/16.py
+++ 2023/16/16.py
@@ -9,7 +9,11 @@ def beam_grid_to_str(beam_grid):
     return '\n'.join([''.join([str(sum(cell)) for cell in row]) for row in beam_grid])
 
 # beam_grid[y][x] = list of beams and their directions passing through (x, y)
-beam_grid = [[[0,0,0,0] for _ in row] for row in mirror_grid]
+def reset_beam_grid():
+    global beam_grid
+    beam_grid = [[[0,0,0,0] for _ in row] for row in mirror_grid]
+
+reset_beam_grid()
 dir_map = dict(zip('UDLR', range(4)))
 
 def propogate_beam(x, y, dir):
@@ -24,7 +28,6 @@ def propogate_beam(x, y, dir):
             continue
 
         if beam_grid[y][x][dir_map[dir]]:
-            print(f"found dupe at ({x}, {y}) in direction {dir}")
             continue
 
         beam_grid[y][x][dir_map[dir]] = 1
@@ -61,9 +64,24 @@ def propogate_beam(x, y, dir):
 def count_energized(beam_grid):
     return sum([sum(cell) > 0 for row in beam_grid for cell in row])
 
-print(grid_to_str(mirror_grid))
+def score_starting_point(x, y, dir):
+    reset_beam_grid()
+    propogate_beam(x, y, dir)
+    return count_energized(beam_grid)
+
+def part2():
+    scores = []
+    scores.extend(score_starting_point(0, y, 'R') for y in range(len(mirror_grid)))
+    scores.extend(score_starting_point(x, 0, 'D') for x in range(len(mirror_grid[0])))
+    scores.extend(score_starting_point(len(mirror_grid[0]) - 1, y, 'L') for y in range(len(mirror_grid)))
+    scores.extend(score_starting_point(x, len(mirror_grid) - 1, 'U') for x in range(len(mirror_grid[0])))
+    return max(scores)
+
+#print(grid_to_str(mirror_grid))
 
 propogate_beam(0, 0, 'R')
-print('\n')
-print(beam_grid_to_str(beam_grid))
+#print('\n')
+#print(beam_grid_to_str(beam_grid))
 print(count_energized(beam_grid))
+
+print(part2())
