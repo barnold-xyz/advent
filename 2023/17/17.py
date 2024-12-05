e809e293e9ff49ef9b1dc0b31ed4dc92ea320fc4
unknown <ben.r.arnold@gmail.com>
2024-12-04 19:06:28 -0500

    day 17 temp

--- 2023/17/17.py
+++ 2023/17/17.py
@@ -1,9 +1,7 @@
 import networkx as nx
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from math import inf
 
-input_file = '2023/17/test_pt2.txt'
+input_file = '2023/17/input.txt'
 dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 '''
@@ -47,33 +45,41 @@ graph2 = nx.DiGraph()
 graph2.add_nodes_from(g)
 print(graph2)
 for (i, j, dir) in g:
-    for di, dj in set(dirs) - {(dir[0], dir[1]),(-dir[0], -dir[1])}:
-        skipped_nodes = [(i+di*dist, j+dj*dist, (di, dj)) for dist in range(1, 4)]
-        skipped_wt = sum(G[n] for n in skipped_nodes if n in g)
-
-        neighbors = [(i+di*dist, j+dj*dist, (di, dj)) for dist in range(4, 11)]
-        [graph2.add_edge((i, j, dir), n, weight=G[n]+skipped_wt) for n in neighbors if n in g]
+    for di, dj in set(dirs) - {(dir[0], dir[1]),(-dir[0], -dir[1])}: # force a turn but not a 180
+        for dist in range(4, 11):
+            neighbor = (i+di*dist, j+dj*dist, (di, dj))
+            skipped_nodes = [(i+di*dd, j+dj*dd, (di, dj)) for dd in range(1, dist)]
+            weight = sum(G[n] for n in skipped_nodes if n in g) + (G[neighbor] if neighbor in g else 0)
+            graph2.add_edge((i, j, dir), neighbor, weight=weight)
 
 print(graph2)
+## add the starting node
+start = (0, 0, (0, 0))
+graph2.add_node(start)
+for i in range(4, 11):
+    wt = sum(G[(0, j, (0, 1))] for j in range(0, i))
+    graph2.add_edge(start, (0, i, (0, 1)), weight=wt)
+print(graph2)
 
-start_nodes = [(0, 0, d) for d in dirs]
 end_nodes = [(max(i for i, j, *_ in g), max(j for i, j, *_ in g), dir) for dir in dirs]
-dist = nx.single_source_dijkstra_path_length(graph2, source=start_nodes[0], weight='weight')
 
-# print the grid: 1 if reachable else 0   
-for i in range(0, 5):
-    continue
-    for j in range(0, 12):
-        print(1 if any([(i, j, d) in dist for d in dirs])  else 0, end='')
-    print()
+dist = nx.single_source_dijkstra_path_length(graph2, source=start, weight='weight')
+
 print(min(dist.get(end, inf) for end in end_nodes))
+
 # print the path
-path = nx.shortest_path(graph2, source=start_nodes[0], target=end_nodes[0], weight='weight')
+path = nx.shortest_path(graph2, source=start, target=end_nodes[0], weight='weight')
 print(path)
+
+# print the weights of each edge in the path
+#print([graph2[path[i]][path[i+1]]['weight'] for i in range(0, len(path)-1)])
+#print(graph2.get_edge_data(path[-2], path[-1]))
+
+'''
 # display the path in the grid
-dir_map = {dirs[0]: '>', dirs[1]: '<', dirs[2]: '^', dirs[3]: 'v'}
-for i in range(0, 5):
-    for j in range(0, 12):
+dir_map = {dirs[0]: '>', dirs[1]: '<', dirs[2]: 'v', dirs[3]: '^'}
+for i in range(0, end_nodes[0][0]+1):
+    for j in range(0, end_nodes[0][1]+1):
          for d in dirs:
              if (i, j, d) in path:
                  print(dir_map[d], end='')
@@ -84,50 +90,4 @@ for i in range(0, 5):
 
 
 quit()
-
-neighbors = set((i, j, d) 
-                for n in start_nodes
-                for i, j, d in graph2.neighbors(n))
-print([n for n in neighbors]) # if (n[0], n[1]) in [(4,10)]])
-
-
-
-
-quit()
-
-start = (0, 0, dirs[0], 0)
-end_nodes2 = [(max(i for i, j, *_ in g), max(j for i, j, *_ in g), dir, consec_steps)
-                for consec_steps in range(0, 10)
-                for dir in dirs]
-dist2 = nx.single_source_dijkstra_path_length(graph2, source=start, weight='weight')
-print(min(dist2.get(end, inf) for end in end_nodes2))
-
-
-
-for i in range(0, 7):
-    continue
-    for j in range(0, 11):
-        print(G[(i, j, dirs[0], 0)], end='')
-    print()
-print('-----------------')
-# print the grid: 1 if reachable else 0   
-for i in range(0, 5):
-    for j in range(0, 12):
-        print(1 if any([(i, j, d, c) in dist2 
-                        for c in range(0, 10)
-                        for d in dirs])  else 0, end='')
-    print()
-
-end_test = [(4, 11, d, c) 
-            for c in range(0, 10)
-            for d in dirs]
-print(min(dist2.get(end, inf) for end in end_test))
-
-# print all the neighbors for a given node set
-neighbors = set((i, j, d, c) 
-                for n in end_test 
-                for i, j, d, c in graph2.neighbors(n))
-print([n for n in neighbors if (n[0], n[1]) in [(4,10)]])
-
-
-#print(end_nodes2)
+'''
