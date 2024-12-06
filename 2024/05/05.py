00cb19e2913aec6cd60c31302fd298671a44a351
Ben Arnold <Ben@Bens-MacBook-Air.local>
2024-12-05 20:52:39 -0500

    2024 day 5 cleanup

--- 2024/05/05.py
+++ 2024/05/05.py
@@ -5,44 +5,24 @@ rule_list = [x.split('|') for x in rule_list]
 pages_list = [x.split(',') for x in pages_list]
 
 # rules will be a dict from the page to the list of pages that must come after it if they are present
-rules = {}
-for first, second in rule_list:
-    if first not in rules:
-        rules[first] = []
-    rules[first].append(second)
+rules = {first: [] for first, _ in rule_list}
+[rules[first].append(second) for first, second in rule_list]
 
 # naiive rule check
 def check_order(pages):
-    # throw an error if there are duplicate pages
-    if len(pages) != len(set(pages)):
-        print(f'Duplicate pages: {pages}')
-    for page in pages:
-        if rules.get(page) is not None:
-            for second in rules[page]:
-                if second in pages:
-                    if pages.index(page) > pages.index(second):
-                        return False
-    return True
+    return all(not (second in pages and pages.index(page) > pages.index(second)) 
+               for page in set(pages).intersection(rules.keys()) 
+               for second in rules[page])
 
 def middle_page(pages):
     return int(pages[len(pages) // 2])
 
 def compare_pages(p1, p2):
-    if rules.get(p1) is not None:
-        if p2 in rules[p1]:
-            return -1
-    if rules.get(p2) is not None:
-        if p1 in rules[p2]:
-            return 1
+    if p2 in rules.get(p1, []): return -1
+    if p1 in rules.get(p2, []): return 1
     return 0
     
-def part1():
-    pages_in_order = [check_order(pages) for pages in pages_list] 
-    return sum(middle_page(pages) for pages, in_order in zip(pages_list, pages_in_order) if in_order)
+pages_in_order = [check_order(pages) for pages in pages_list]
 
-def part2():
-    pages_in_order = [check_order(pages) for pages in pages_list] 
-    return sum(middle_page(sorted(pages, key=cmp_to_key(compare_pages))) for pages, in_order in zip(pages_list, pages_in_order) if not in_order)
-
-print(part1())
-print(part2())
+print(sum(middle_page(pages) for pages, in_order in zip(pages_list, pages_in_order) if in_order))
+print(sum(middle_page(sorted(pages, key=cmp_to_key(compare_pages))) for pages, in_order in zip(pages_list, pages_in_order) if not in_order))
