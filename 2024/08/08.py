49563ed36e3de2349daea90cbe1d762de40c9c5a
Ben Arnold <Ben@Bens-MacBook-Air.local>
2024-12-08 12:21:04 -0500

    day 8 pt2

--- 2024/08/08.py
+++ 2024/08/08.py
@@ -1,11 +1,13 @@
 import itertools
 
 grid = {(x, y): char for y, line in enumerate(open("2024/08/input.txt").read().split('\n')) for x, char in enumerate(line)}
+width = max(x for x, y in grid.keys()) + 1
+height = max(y for x, y in grid.keys()) + 1
 
 def print_grid(grid, antinodes=set()):
     print()
-    for y in range(max(y for x, y in grid.keys()) + 1):
-        for x in range(max(x for x, y in grid.keys()) + 1):
+    for y in range(width):
+        for x in range(height):
             if (x, y) in antinodes:
                 print('X', end='')
             else:
@@ -26,6 +28,26 @@ def add_antinodes(source1, source2):
     if anti1 in grid: antinodes.add(anti1)
     if anti2 in grid: antinodes.add(anti2) 
 
+antinodes_pt2 = set()
+def add_antinodes_pt2(source1, source2):
+    xdiff = source2[0] - source1[0]
+    ydiff = source2[1] - source1[1]
+
+    # start at source1 and move right until the end of the grid
+    for i in range(0, width):
+        node = (source1[0] + i*xdiff, source1[1] + i*ydiff)
+        if node in grid: 
+            antinodes_pt2.add(node)
+        else: 
+            break
+    # now move left until the end of the grid
+    for i in range(0, width):
+        node = (source1[0] - i*xdiff, source1[1] - i*ydiff)
+        if node in grid: 
+            antinodes_pt2.add(node)
+        else: 
+            break
+
 # find all pairs of nodes that have the same grid value
 def find_pairs(grid):
     antenna_loc = {char: [pos for pos, c in grid.items() if c == char] for char in set(grid.values()) if char != '.'}
@@ -34,14 +56,17 @@ def find_pairs(grid):
     #print(pairs)
     return pairs
 
-def add_all_antinodes(grid):
+def add_all_antinodes(grid, fn=add_antinodes):
     pairs = find_pairs(grid)
     for char, locs in pairs.items():
         for loc1, loc2 in locs:
-            add_antinodes(loc1, loc2)
+            fn(loc1, loc2)
 
 #print(grid)
 #print_grid(grid)
 add_all_antinodes(grid)
 #print_grid(grid, antinodes)
 print(len(antinodes))
+add_all_antinodes(grid, fn=add_antinodes_pt2)
+print(len(antinodes_pt2))
+#print_grid(grid, antinodes_pt2)
