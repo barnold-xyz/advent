5a569131171917b0952ca308b0ae220f18a2ba0f
Ben Arnold <Ben@Bens-MacBook-Air.local>
2024-12-08 11:16:03 -0500

    day 7 pt2

--- 2024/07/07.py
+++ 2024/07/07.py
@@ -5,7 +5,9 @@ data = open("2024/07/input.txt").read().split('\n')
 
 def add(x,y): return x+y
 def mult(x,y): return x*y
-fn_list = [add, mult]
+def concat(x,y): return int(str(x) + str(y))
+fn_list_part1 = [add, mult]
+fn_list_part2 = [add, mult, concat]
 
 def parse_line(line):
     target, args = line.split(':')
@@ -13,7 +15,7 @@ def parse_line(line):
     args = list(map(int, args.strip().split(' ')))
     return target, args
 
-def test_line(line):
+def test_line(line, fn_list):
     target, args = parse_line(line)
     stack = [args]
     while stack:
@@ -21,9 +23,10 @@ def test_line(line):
         if len(current) == 1:
             if current[0] == target:
                 return target
-        else: 
-            [stack.append([fn(current[0], current[1])] + current[2:]) for fn in fn_list]
+        else:
+            if current[0] <= target: 
+                [stack.append([fn(current[0], current[1])] + current[2:]) for fn in fn_list]
     return 0
     
-print(sum(test_line(line) for line in data))
-
+print(sum(test_line(line, fn_list_part1) for line in data))
+print(sum(test_line(line, fn_list_part2) for line in data))
