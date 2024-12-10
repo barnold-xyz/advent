12a2f3160d63d201f15a5e7734df289e9b048111
unknown <ben.r.arnold@gmail.com>
2024-12-10 10:57:07 -0500

    2024 day 10 pt2

--- 2024/10/10.py
+++ 2024/10/10.py
@@ -1,24 +1,21 @@
+from collections import defaultdict
 
 graph = {(row, col): int(cell) for row, line in enumerate(open("2024/10/input.txt").readlines()) for col, cell in enumerate(line.strip())}
 trailheads = [(row, col) for row, col in graph if graph[(row, col)] == 0]
 
-#print(graph)
-#print(trailheads)
-
 def explore_trail(trailhead):
     trail = [trailhead]
-    endpoints = set()
+    endpoints = defaultdict(int)
     while trail:
         row, col = trail.pop()
         if graph[(row, col)] == 9:
-            print(f'reached end! {row, col}')
-            endpoints.add((row, col))
+            endpoints[(row, col)] += 1
         for d in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
             new_row, new_col = row + d[0], col + d[1]
             if graph.get((new_row, new_col), '') == graph[(row, col)] + 1:
-                #print('moving to', new_row, new_col, graph[(new_row, new_col)])
                 trail.append((new_row, new_col))
     return endpoints
 
-print(sum(len(explore_trail(trailhead)) for trailhead in trailheads))
-
+trail_info = [explore_trail(trailhead) for trailhead in trailheads]
+print(sum(len(info) for info in trail_info))
+print(sum(sum(info.values()) for info in trail_info))
