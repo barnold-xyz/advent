1c338439cd3c5777b9f01cbbac454712eaa9d131
unknown <ben.r.arnold@gmail.com>
2024-12-11 12:35:39 -0500

    2024 day 11 pt2 best

--- 2024/11/11.py
+++ 2024/11/11.py
@@ -2,89 +2,11 @@ import cProfile
 import pstats
 import io
 import copy
-from collections import defaultdict
 
 #data = '125 17'
 data = '890 0 1 935698 68001 3441397 7221 27'
 
 stones = [int(x) for x in data.split()]
-
-def blink(stones):
-    new_stones = []
-    for s in stones:
-        if s == 0:
-            new_stones.append(1)
-        elif len(str(s)) % 2 == 0:
-            str_s = str(s)
-            new_stones.append(int(str_s[0:len(str_s)//2]))
-            new_stones.append(int(str_s[len(str_s)//2:]))
-        else:
-            new_stones.append(s*2024)
-    return new_stones
-
-def pt1(stones):
-    print(f'0: {stones}')
-    for i in range(1, 25+1):
-        stones = blink(stones)
-        if i <= 6:
-            print(f'{i}: {stones}')
-        if i == 6: 
-            print(len(stones))
-    print(len(stones))
-
-def pt2(stones):
-    for i in range(1, 75+1):
-        print(i)
-        stones = blink(stones)
-    print(len(stones))
-
-def investigate():
-    stones = [2024]
-    for i in range(1, 10+1):
-        stones = blink(stones)
-        print(f'{i}: {len(stones)}')
-        if 2024 in stones:
-            print(f'2024 found in {i} iterations: {stones}')
-            break
-
-    stones = [40]
-    print(f'checking 40')
-    for i in range(1, 10+1):
-        stones = blink(stones)
-        print(f'{i}: {len(stones)}')
-        if 2024 in stones:
-            print(f'2024 found in {i} iterations: {stones}')
-            break
-
-def apply_rule(s):
-    if s == 0:
-        return [1]
-    if len(str(s)) % 2 == 0:
-        str_s = str(s)
-        l = len(str_s) // 2
-        return [int(str_s[0:l]), int(str_s[l:])]
-    return [s*2024]
-
-def loops_til_2024():
-    loops = {}
-    start_stone = 2024
-    stones_2024 = [start_stone]
-    for i in range(1, 30):
-        stones_2024 = blink(stones_2024)
-        print(f'{i}: {len(stones_2024)}, {stones_2024}')
-        if 2024 in stones_2024:
-            print(f'2024 found in {i} iterations: {stones_2024}')
-            break
-
-    for s in stones_2024:
-        stones = [s]
-        for i in range(1, 30):
-            stones = blink(stones)
-            if 2024 in stones:
-                loops[s] = (i, stones)
-                break
-    [print(f'{k}: {v}') for k,v in loops.items()]
-    return loops
 
 # define a graph where each node is a stone and each edge is a transformation from one stone to another
 # the transformation is a split if the stone is even, a multiply by 2024 if the stone is odd, and a change to 1 if the stone is 0
@@ -183,7 +105,6 @@ pr.disable()
 
 # Print profiling results
 s = io.StringIO()
-sortby = 'cumulative'
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
 ps.print_stats()
 print(s.getvalue())
