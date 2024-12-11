b8e6e4b57e5e4ca639d3660bb631b56f8630eba2
unknown <ben.r.arnold@gmail.com>
2024-12-11 17:07:08 -0500

    2024 day 24 pt2

--- 2023/24/24.py
+++ 2023/24/24.py
@@ -1,3 +1,6 @@
+import numpy as np
+from z3 import *
+
 data = [[int(n.strip()) for n in line.split(',')] for line in open("2023/24/input.txt").read().replace('@', ',').split('\n')]
 
 data = [[(a, b, c), (x, y, z)] for a, b, c, x, y, z in data]
@@ -24,7 +27,7 @@ def intersects_xy(a, b):
 
     return (x, y, 0)
 
-def test_intersection(a, b, lbound, ubound):
+def test_intersection(a, b, lbound, ubound, check_z=False):
     (ax, ay, az), (avx, avy, avz) = a
     (bx, by, bz), (bvx, bvy, bvz) = b
     
@@ -41,7 +44,14 @@ def test_intersection(a, b, lbound, ubound):
     time_a = (intersection[0] - ax) / avx
     time_b = (intersection[0] - bx) / bvx
 
-    return time_a > 0 and time_b > 0
+    if not(time_a > 0 and time_b > 0):
+        return False
+    
+    if check_z:
+        if not az + time_a * avz == bz + time_b * bvz:
+            return False
+        
+    return True
 
 def test(data):
     lbound = 7
@@ -58,8 +68,37 @@ def part1(data):
     results = []
     for i in range(len(data)):
         for j in range(i):
-            results.append(test_intersection(data[i], data[j], lbound, ubound))
+            results.append(test_intersection(data[i], data[j], lbound, ubound, check_z=False))
     print(sum(results))
+
+# thanks claude
+def part2(data):
+    # Create symbolic variables for rock's initial position and velocity
+    rx, ry, rz = Ints('rx ry rz')
+    rvx, rvy, rvz = Ints('rvx rvy rvz')
+    
+    # Create solver
+    s = Solver()
+    
+    # For each hailstone, create time variable and add constraints
+    for i, ((hx, hy, hz), (hvx, hvy, hvz)) in enumerate(data):
+        t = Int(f't{i}')
+        
+        # Collision equations for x, y, z dimensions
+        s.add(t >= 0)
+        s.add(rx + rvx * t == hx + hvx * t)
+        s.add(ry + rvy * t == hy + hvy * t)
+        s.add(rz + rvz * t == hz + hvz * t)
+    
+    # Check if solution exists
+    if s.check() == sat:
+        m = s.model()
+        return sum([m.evaluate(rx).as_long(), 
+                    m.evaluate(ry).as_long(), 
+                    m.evaluate(rz).as_long()])
+    
+    return None
 
 #test(data)
 part1(data)
+print(part2(data))
